dc3b43ccb11b0c57ae090260dd6776b16c9a2512
abdullahejazjanjua <abdullahejaz822@gmail.com>
2026-02-01 20:28:20 +0500

    added vectoradd kernel

---
+++ vectorAdd/vectorAdd.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+__global__ void vectorAddKernel(float *A, float *B, float *C, int N)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (i < N)
+    {
+        C[i] = A[i] + B[i];
+    }
+}
+
+void vectorAdd(float *A_h, float *B_h, float *C_h, int N)
+{
+    float *A_d, *B_d, *C_d;
+    int byteSize = N * sizeof(float);
+    
+    cudaMalloc((void **)&A_d, byteSize);
+    cudaMalloc((void **)&B_d, byteSize);
+    cudaMalloc((void **)&C_d, byteSize);
+    
+    cudaMemcpy(A_d, A_h, byteSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, byteSize, cudaMemcpyHostToDevice);
+    
+    vectorAddKernel<<<N/256, 256>>>(A_d, B_d, C_d, N);
+    
+    cudaMemcpy(C_d, C_h, byteSize, cudaMemcpyDeviceToHost);
+    
+    
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+
+}
