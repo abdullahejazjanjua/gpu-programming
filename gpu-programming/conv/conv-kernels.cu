cb1fdd73d87438fea1d5343b5a401d195c513874
abdullahejazjanjua <abdullahejaz822@gmail.com>
2026-03-07 19:07:38 +0500

    added all versions of conv

--- gpu-programming/conv/conv-kernels.cu
+++ gpu-programming/conv/conv-kernels.cu
@@ -47,6 +47,68 @@ __global__ void ConvKernel2(float *datain, float *dataout, int n, int m) {
     }
 }
 
+__global__ void ConvKernel3(float *datain, float *dataout, int n, int m) {
+    __shared__ float datain_tile[IN_DIM][IN_DIM];
+    
+    int row = blockIdx.y * OUT_DIM + threadIdx.y - FILTER_RADIUS;
+    int col = blockIdx.x * OUT_DIM + threadIdx.x - FILTER_RADIUS;
+    
+    if ((row >= 0 && row < n) && (col >= 0 && col < m))
+        datain_tile[threadIdx.y][threadIdx.x] = datain[row * m + col];
+    else
+        datain_tile[threadIdx.y][threadIdx.x] = 0.0f;
+    __syncthreads();
+    
+    
+    int tilerow = threadIdx.y - FILTER_RADIUS;
+    int tilecol = threadIdx.x - FILTER_RADIUS;
+    if (    ((row >= 0 && row < n) && (col >= 0 && col < m)) && 
+        ((tilerow >= 0 && tilerow < OUT_DIM) && (tilecol >= 0 && tilecol < OUT_DIM))    ) {
+        float pvalue = 0.0f;
+        
+        for (int fi = 0; fi < FILTER_SIZE; fi++) {
+            for (int fj = 0; fj < FILTER_SIZE; fj++) {
+                pvalue += (datain_tile[tilerow + fi][tilecol + fj] * FILTER_KERNEL[fi][fj]);
+            }
+        }  
+        dataout[row * m + col] = pvalue;
+    }   
+}
+
+__global__ void ConvKernel4(float *datain, float *dataout, int n, int m) {
+    __shared__ float datain_tile[IN_DIM][IN_DIM];
+    
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if ((row >= 0 && row < n) && (col >= 0 && col < m))
+        datain_tile[threadIdx.y][threadIdx.x] = datain[row * m + col];
+    else
+        datain_tile[threadIdx.y][threadIdx.x] = 0.0f;
+    __syncthreads();
+    
+    
+    if (row < n && col < m) {
+        float pvalue = 0.0f;
+        
+        for (int fi = 0; fi < FILTER_SIZE; fi++) {
+            for (int fj = 0; fj < FILTER_SIZE; fj++) {
+                if ( ((int) threadIdx.y - FILTER_RADIUS + fi >= 0) && (threadIdx.y - FILTER_RADIUS + fi < IN_DIM)  && 
+                     ((int) threadIdx.x - FILTER_RADIUS + fj >= 0) && (threadIdx.x - FILTER_RADIUS + fj < IN_DIM)
+                   )
+                    pvalue += (datain_tile[threadIdx.y - FILTER_RADIUS + fi][threadIdx.x - FILTER_RADIUS + fj] * FILTER_KERNEL[fi][fj]);
+                // our hope is that the halo cells will most likely be in cache
+                else if ( (row - FILTER_RADIUS + fi >= 0) && (row - FILTER_RADIUS + fi < n)  && 
+                          (col - FILTER_RADIUS + fj >= 0) && (col - FILTER_RADIUS + fj < m)
+                        )
+                            pvalue += (datain[(row - FILTER_RADIUS + fi) * m + (col - FILTER_RADIUS + fj)] * FILTER_KERNEL[fi][fj]);                
+            }
+        }  
+        dataout[row * m + col] = pvalue;
+    }   
+}
+
+
 int cdiv(int size, int block_size) {
     return (size + block_size - 1) / block_size;
 }
@@ -81,6 +143,15 @@ void ConvGpu(float *datain, float *dataout, float *filter, int n, int m, int con
         case 2:
             ConvKernel2<<<dimGrid, dimBlock>>>(datain_d, dataout_d, n, m);
             break;
+        case 3: {
+            dim3 dimGrid(cdiv(m, OUT_DIM), cdiv(n, OUT_DIM)); // we need to launch more blocks as each block effectively 
+                                                              // computes only OUT_DIM x OUT_DIM elements instead of BLOCK_SIZE x BLOCK_SIZE
+            ConvKernel3<<<dimGrid, dimBlock>>>(datain_d, dataout_d, n, m);
+            break;
+        }
+        case 4:
+            ConvKernel4<<<dimGrid, dimBlock>>>(datain_d, dataout_d, n, m);
+            break;
         default:
             fprintf(stderr, "No such kernel version found\n");
             exit(EXIT_FAILURE);
@@ -100,5 +171,5 @@ void ConvGpu(float *datain, float *dataout, float *filter, int n, int m, int con
   
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
-    printf("    GPU TIME: %f microsecs\n", milliseconds * 1000);
+    printf("GPU TIME: %f microsecs\n", milliseconds * 1000);
 }
